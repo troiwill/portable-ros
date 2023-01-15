4e61eeedfeb3692c0321ded66a162c35d079c8be
Troi Williams <40696868+troiwill@users.noreply.github.com>
2023-01-15 18:02:10 -0500

    Added readme and setup script.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# portable-ros
+# Portable ROS Setups
+
+## Overview
+
+This repository contains instructions for setting up ROS on Linux and Mac OS using conda. The instructions were derived from [Robostack](https://robostack.github.io/index.html). The setup script creates a conda environment for ROS Noetic Desktop. We recommend that you create a copy of this conda environment for your projects and add any additional (conda or ROS) packages you require for your project.
+
+This script has been tested on Mac OS X (M1) hosts.
+
+## Setup
+
+Follow the instructions below to create the ROS conda environment. **Note** that we assume you already installed [anaconda](https://www.anaconda.com) or [miniconda](https://docs.conda.io/en/latest/miniconda.html) on your system.
+
+To run the setup script in `conda-setup`, run the following lines:
+```
+# NOTE: We assume you're in the directory for this repository.
+# Make the script executable.
+cd conda-setup
+chmod +x setup-ros-noetic.sh
+
+# Create the ROS environment.
+./setup-ros-noetic.sh
+
+# Activate the new environment.
+conda activate ros-noetic
+```
+
+You can clone this environment using the following command:
+```
+conda create --name ros-noetic-clone --clone ros-noetic
+```
+You should substitute the name `ros-noetic-clone` for a name you like. For example, you can name the clone the name of your current ROS project.
---
+++ conda-setup/setup-ros-noetic.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+CONDA_INSTALL_DIR=$(conda info | grep -i 'base environment' | grep -Eo '(/[a-zA-Z0-9]+)+')
+ROS_DISTRO="noetic"
+CONDA_ROS_BASE_ENV="ros-${ROS_DISTRO}"
+
+exit_on_error() {
+    echo "failed."
+    exit 1
+}
+
+# Source the conda script.
+source "${CONDA_INSTALL_DIR}/etc/profile.d/conda.sh"
+
+# Install the mamba package.
+echo "Installing the mamba command if it does not exist."
+conda activate base || exit_on_error
+conda install -y mamba -c conda-forge || exit_on_error
+
+# Create the base environment for ROS.
+echo "Create the base environment for ROS ${ROS_DISTRO}."
+mamba create -n ${CONDA_ROS_BASE_ENV} -y python=3.9
+conda activate ${CONDA_ROS_BASE_ENV}
+conda config --env --add channels conda-forge
+conda config --env --add channels robostack
+conda config --env --add channels robostack-experimental
+mamba install -y ros-noetic-desktop || exit_on_error
+
+mamba install -y compilers cmake pkg-config make ninja || exit_on_error
+mamba install -y catkin_tools || exit_on_error
+
+# only on linux, uncomment lines if you are having issues finding GL/OpenGL, also do:
+# mamba install mesa-libgl-devel-cos7-x86_64 \
+#     mesa-dri-drivers-cos7-x86_64 \
+#     libselinux-cos7-x86_64 \
+#     libxdamage-cos7-x86_64 \
+#     libxxf86vm-cos7-x86_64 \
+#     libxext-cos7-x86_64 \
+#     xorg-libxfixes || exit_on_error
+
+conda deactivate || exit_on_error
+conda activate ${CONDA_ROS_BASE_ENV} || exit_on_error
+
+mamba install -y rosdep || exit_on_error
+rosdep init || exit_on_error
+rosdep update || exit_on_error
+
+echo ""
+echo ""
+echo "Base conda environment for ROS is now set up!"
+echo "You can activate your environment by running:"
+echo ""
+echo "    conda activate ${CONDA_ROS_BASE_ENV}"
+echo ""
